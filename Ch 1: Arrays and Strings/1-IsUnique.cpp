1614d5ce5210f1057772105ce64f1e21d33486ab
gargimahale <gargimahale102@gmail.com>
2021-08-20 22:41:05 -0400

    Added CTCI files

--- Ch 1: Arrays and Strings/1-IsUnique.cpp
+++ Ch 1: Arrays and Strings/1-IsUnique.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 /*
@@ -11,38 +14,38 @@ What if you cannot use additional data structures?
 // Unicode (1,114,112 characters) -> from 0x000000 to 0x10FFFF
 
 // In this case just assuming ASCII Set of 128 Chars
+class Solution{
+public:
+    bool isUnique(string& str) {
+        if (str.size() > 128) return false;
 
-bool isUnique(string& str) {
-    if (str.size() > 128) return false;
-
-    vector<bool> check(128, false);
-    for (char ch : str) {
-        if (check[ch]) return false;
-        check[ch] = true;
-    }
-    return true;
-}
-
-// OR
-
-// Reduces space usage but in worst case gives a complexity of O(n^2)
-
-bool isUnique(string& str) {
-    int n = str.size(), check = 0;
-    for (int i = 0; i < n; ++i) {
-        int val = str[i] - 'a';
-        if ((check & (1 << val))) {
-            return false;
+        vector<bool> check(128, false);
+        for (char ch : str) {
+            if (check[ch]) return false;
+            check[ch] = true;
         }
-        check |= (1 << val);
-    }
-    return true;
-}
+        return true;
+    }   
 
-int main() {
+    // Reduces space usage but in worst case gives a complexity of O(n^2)
+    bool isUnique_2(string& str) {
+        int n = str.size(), check = 0;
+        for (int i = 0; i < n; ++i) {
+            int val = str[i] - 'a';
+            if ((check & (1 << val))) {
+                return false;
+            }
+            check |= (1 << val);
+        }
+        return true;
+    }   
+};
+
+int32_t main() {
+    Solution sol;
     string s;
     cin >> s;
-    cout << isUnique(s) << "\n";
+    cout << sol.isUnique(s) << "\n";
     return 0;
 }
 // TC: O(1) - As we will never iterate through more than 128 chars
